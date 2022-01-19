38bf2f24cc4c72819171d23dbb784cb0a11643ac
MartenAlan <martenalan97@yahoo.de>
2022-01-19 15:01:41 +0100

    Ausgabefeld, um Inputs erweitert

--- qt_ical/mainwindow.cpp
+++ qt_ical/mainwindow.cpp
@@ -17,8 +17,13 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     QString ics_str = ui->label_id->text() + "\n" + ui->label_version->text() + "\n" + ui->prod_id->text() + "\n";
+    QString ics_str2 = ui->titel->toPlainText() + "\n" + ui->beschreibung->toPlainText();
 
+    QDate ics_dtstart = ui->input_dtstart->date();
+    QDate ics_dtend = ui->input_dtend->date();
 
-    ui->output->setText(ics_str);
+    QString dstart_dtend = ics_dtstart.toString() + "->" + ics_dtend.toString();
+
+    ui->output->setText(ics_str + "\n" + ics_str2 + "\n" + dstart_dtend);
 }
 
