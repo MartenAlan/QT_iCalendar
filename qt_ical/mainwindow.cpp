0c049e0628049095a04ca04da9d2fc5e8c965cc9
Stefan Deltchev <stefan.deltchev@gmail.com>
2022-01-21 08:39:15 +0100

    Ausgabe in QMessageBox

--- qt_ical/mainwindow.cpp
+++ qt_ical/mainwindow.cpp
@@ -89,10 +89,10 @@ void MainWindow::on_pushButton_clicked()
     FILE *o_file = fopen(filename.toUtf8(), "w+");
     if (o_file){
         fwrite(ical.buildICSText().c_str(), 1, ical.buildICSText().size(), o_file);
-        ui->output->setText("Done Writing!");
+        QMessageBox::information(this, "iCal", "Done Writing!");
     }
     else{
-        ui->output->setText("Something went wrong!");
+        QMessageBox::critical(this, "iCal", "Something went wrong!");
     }
     fclose(o_file);
 
